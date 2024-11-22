57ef80679d4f6c102f18aeac30354dddc7e6f7c7
Bhagya54 <testingtrainer54@gmail.com>
2024-11-22 23:13:44 +0530

    Initial commit

---
+++ src/Day1/Array_LeftRotation.java
@@ -0,0 +1,31 @@
+package Day1;
+
+public class Array_LeftRotation {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0]=45;
+        a[1]=20;
+        a[2]=7;
+        a[3]=15;
+        a[4]=40;
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            System.out.print(a[i] + " ");
+        }
+        int rotationCount=3;
+        for(int j=0;j<rotationCount;j++){
+            int temp=a[0];
+            for(int i=0;i<n-1;i++){
+                a[i]=a[i+1];
+            }
+            a[n-1]= temp;
+            System.out.println();
+            System.out.println("After "+ (j+1)+" anti clock wise Rotation");
+            for(int i=0;i<a.length;i++){
+                System.out.print(a[i] + " ");
+            }
+        }
+        System.out.println();
+
+    }
+}
---
+++ src/Day1/Array_ValuesSquare.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Array_ValuesSquare {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0]=45;
+        a[1]=20;
+        a[2]=7;
+        a[3]=15;
+        a[4]=40;
+        for(int i=0;i<a.length;i++){
+            a[i] = a[i]*a[i];//SC: i(auxillary space): o(1), TC: 1*n = o(n)
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+ " ");
+        }
+
+    }
+
+}
